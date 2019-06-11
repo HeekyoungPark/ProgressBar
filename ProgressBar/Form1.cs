f6bf068629b7cb6930a79a8024e4de76ee85230a
Heekyoung Park <acdong0202@gmail.com>
2019-06-11 14:30:51 +0900

    #1 진생상태바 폼 디자인

--- ProgressBar/Form1.cs
+++ ProgressBar/Form1.cs
@@ -15,5 +15,30 @@ namespace ProgressBar
         {
             InitializeComponent();
         }
+
+        private void BtnRun_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnReset_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnEnd_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
